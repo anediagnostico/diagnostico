b63725b8a2be3344652792257ace10e5a7895048
danilson-dnlsn <danilson.barros@novaescola.org.br>
2025-03-13 10:37:20 -0300

    adicionando grágico com numero de professores que realizaram sondagem por mes

--- pages/(3)_turmas_com_melhoria.py
+++ pages/(3)_turmas_com_melhoria.py
@@ -258,7 +258,7 @@ df_sondagens_diarias = turmas.groupby(turmas['mes_sondagem']).size().reset_index
 df_sondagens_diarias = df_sondagens_diarias.sort_values(by='mes_sondagem')
 
 fig_sondagens_diarias = go.Figure(data=[go.Scatter(x=df_sondagens_diarias['mes_sondagem'], y=df_sondagens_diarias['total_sondagens'])])
-fig_sondagens_diarias.update_layout(title='Número de Sondagens Realizadas', xaxis_title='Mês', yaxis_title='Número de Sondagens')
+fig_sondagens_diarias.update_layout(title='Número de Sondagens Realizadas por Mês', xaxis_title='Mês', yaxis_title='Número de Sondagens')
 
 st.plotly_chart(fig_sondagens_diarias, use_container_width=True)
 
@@ -267,6 +267,6 @@ df_professores_sondagens_diarias = turmas.groupby(turmas['mes_sondagem'])['id_pr
 df_professores_sondagens_diarias = df_professores_sondagens_diarias.sort_values(by='mes_sondagem')
 
 fig_professores_sondagens_diarias = go.Figure(data=[go.Bar(x=df_professores_sondagens_diarias['mes_sondagem'], y=df_professores_sondagens_diarias['total_professores'])])
-fig_professores_sondagens_diarias.update_layout(title='Número de Professores que Realizaram Sondagens por Dia', xaxis_title='Mês', yaxis_title='Número de Professores')
+fig_professores_sondagens_diarias.update_layout(title='Número de Professores(únicos) que Realizaram Sondagens por Mês', xaxis_title='Mês', yaxis_title='Número de Professores')
 
 st.plotly_chart(fig_professores_sondagens_diarias, use_container_width=True)
